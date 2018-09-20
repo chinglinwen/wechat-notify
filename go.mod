73253d772bc046f25debfdf9238fa5e3360e0377
wenzhenglin <wenzhenglin@qianbao.com>
2018-09-20 16:38:36 +0800

    move cache to a package and use that package

--- go.mod
+++ go.mod
@@ -1,10 +1,26 @@
 module wen/wechat-notify
 
 require (
+	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20180319063640-eb0e8e892f3a // indirect
+	github.com/aws/aws-sdk-go v1.15.38 // indirect
+	github.com/chinglinwen/checkup v0.3.2
 	github.com/chinglinwen/log v0.0.0-20180802093412-402fdc33bf76
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/jmoiron/sqlx v0.0.0-20180614180643-0dae4fefe7c0 // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/miekg/dns v1.0.9 // indirect
+	github.com/moul/http2curl v0.0.0-20170919181001-9ac6cf4d929b // indirect
 	github.com/namsral/flag v1.7.4-pre
+	github.com/parnurzeal/gorequest v0.2.15 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/tidwall/gjson v1.1.3
 	github.com/tidwall/match v0.0.0-20171002075945-1731857f09b1 // indirect
+	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be // indirect
 	gopkg.in/resty.v1 v1.9.1
 )
--- main.go
+++ main.go
@@ -4,27 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
 	"wen/wechat-notify/wechat"
 
+	"github.com/chinglinwen/checkup/cache"
 	"github.com/chinglinwen/log"
-	"github.com/namsral/flag"
 )
-
-var (
-	defaultExpire         = flag.String("e", "10m", "default expire time")
-	defaultExpireDuration time.Duration
-)
-
-func init() {
-	flag.Parse()
-
-	var err error
-	defaultExpireDuration, err = time.ParseDuration(*defaultExpire)
-	if err != nil {
-		log.Fatalf("default expire: %v parse err: %v\n", *defaultExpire, err)
-	}
-}
 
 // test
 // curl "localhost:8001/?user=wenzhenglin&content=aa"
@@ -34,6 +18,8 @@ func sendmsg(w http.ResponseWriter, req *http.Request) {
 	expire := req.FormValue("expire")
 
 	content := req.FormValue("content")
+	status := req.FormValue("status")
+
 	if user == "" || content == "" {
 		e := fmt.Sprintf("user: %v ,or content: %v is empty\n", user, content)
 		log.Printf(e)
@@ -41,7 +27,7 @@ func sendmsg(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if d, found := cacheGet(user, content); found {
+	if d, found := cache.Get(user, content, status); found {
 		e := fmt.Sprintf("user: %v, content: %v not expired in %v, skip send\n",
 			user, content, d.Format("15:04:05"))
 		log.Printf(e)
@@ -49,29 +35,22 @@ func sendmsg(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	var d time.Duration
+	// default should have no cache, send everytime
 	if expire != "" {
-		var err error
-		d, err = time.ParseDuration(expire)
-		if err != nil {
-			e := fmt.Errorf("user: %v, expire: %v parse err: %v, ignore it\n", user, expire, err)
-			log.Println(e)
-		}
-	} else {
-		d = defaultExpireDuration
+		log.Printf("user %v,%v, status: %v, expire set to %v\n", user, content, status, expire)
+		cache.Set(user, content, status, expire)
 	}
-	log.Printf("user %v expire set to %v\n", user, d)
-	cacheSet(user, content, d)
 
+	content += " " + status
 	var msg string
 	_, err := wechat.Sends(users, content)
 	if err != nil {
-		msg = fmt.Sprintf("send err: %v\n", err)
+		msg = fmt.Sprintf("send user: %v, %v, status: %v, expire: %v, err: %v\n", user, content, status, expire, err)
 		log.Printf(msg)
 		fmt.Fprintf(w, msg)
 		return
 	}
-	msg = fmt.Sprintf("send ok: %v, %v\n", user, content)
+	msg = fmt.Sprintf("send ok: %v, %v, status: %v, expire: %v\n", user, content, status, expire)
 	log.Printf(msg)
 	fmt.Fprintf(w, msg)
 	return
